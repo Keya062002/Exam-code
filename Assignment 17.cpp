140c3178e93a711440991a9a741157b770f2a15a
Khadizatul Kubra Keya <163034377+Keya062002@users.noreply.github.com>
2024-03-12 06:33:26 +0600

    Create Assignment 17.cpp

---
+++ Assignment 17.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+class calculation{
+};
+void calculation :: menu(){
+    cout<<"*******MENU*******";
+    cout<<"\nAddition = '+'";
+    cout<<"\nSubtraction = '-'";
+    cout<<"\nMultiplication = '*'";
+    cout<<"\nDivition = '/'";
+    cout<<"\nMake your chiose = ";
+    cin>>x;
+    cout<<"Enter two number = ";
+    cin>>a>>b;
+    switch(x){
+        case '+':
+        cout<<a + b;
+        break;
+        case '-':
+        cout<<a - b;
+        break;
+        case '*':
+        cout<<a * b;
+        break;
+        case '/':
+        cout<<a / b;
+        break;
+        default :
+        cout<<"Error ! Operator is not correct";
+
+
+    }
+}
+int main()
+{
+    calculation p;
+    p.menu();
+
+return 0;
+
+
+}
